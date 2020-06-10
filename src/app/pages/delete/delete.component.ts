66c4ed86e15c94f4cb1ab3432af8fa688dc209fb
Alex-bensimon <64471842+Alex-bensimon@users.noreply.github.com>
2020-06-10 12:48:32 +0200

    delete fn

--- src/app/pages/delete/delete.component.ts
+++ src/app/pages/delete/delete.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Ressource } from '../consult/Ressource';
+import { ApiService } from 'src/service/api.service';
+import { FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-delete',
@@ -7,9 +11,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeleteComponent implements OnInit {
 
-  constructor() { }
-
+deleteForm;
+  constructor(private apiService: ApiService,
+              private formBuilder: FormBuilder,
+              private router: Router
+    ) {
+      this.deleteForm = this.formBuilder.group({
+        id: ''
+      });
+     }
+ 
   ngOnInit(): void {
   }
-
-}
+ 
+  onSubmit(ressource: Ressource) {
+    this.apiService.deleteRessource(ressource.id)
+    .subscribe(
+      data => {
+        this.deleteForm.reset();
+        this.router.navigate(['/consult']);
+      },
+        error => {
+          alert('Error' + error.error);
+          console.log('Error occured', error);
+        }
+    ); }
+  }
